88e0b522231f6935975fe2367adc1f9b9afb0333
Furqaan Malik <129623870+Furqaan09@users.noreply.github.com>
2026-07-29 11:45:38 -0300

    Add evaluation with failure analysis by size and habitat

--- src/metrics.py
+++ src/metrics.py
@@ -8,8 +8,8 @@ def to_prediction(output):
 
 def iou_score(pred, target, eps=1e-7):
     """IoU for the fish class between two [B, H, W] masks of 0s and 1s."""
-    pred_fish = pred == 1
-    true_fish = target == 1
+    pred_fish = (pred == 1)
+    true_fish = (target == 1)
 
     intersection = (pred_fish & true_fish).sum().float()
     union = (pred_fish | true_fish).sum().float()
